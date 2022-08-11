6aeaa105440ebb6c3b1b1540e912182c823ab2e4
Bogem12 <75928934+bogemdesign@users.noreply.github.com>
2022-08-11 23:09:14 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let num = document.querySelector('#number')
-let text = document.querySelector('#text').textContent
+let text = document.querySelector('#text')
 let tombol = num.nextElementSibling.nextElementSibling
 tombol.addEventListener('click', function(){
   let number = num.value
-   window.open(`https://wa.me/${number}/?text=${text}`)
+  let pesan = text.value
+   window.open(`https://wa.me/${number}/?text=${pesan}`)
       location.reload()
       
 })
